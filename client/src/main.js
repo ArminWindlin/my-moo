735d0821db04a1d3bf3c91010bc6d7adeb8bbede
ArminWindlin <armin.windlin@gmail.com>
2020-10-26 15:24:09 +0100

    dynamic api url

--- client/src/main.js
+++ client/src/main.js
@@ -8,6 +8,8 @@ import './scss/global.scss';
 const app = createApp(App);
 
 app.config.globalProperties.$myGlobalVariable = 'test';
+app.config.globalProperties.$api = 'http://localhost:3000/';
+if (process.env.NODE_ENV === 'production') app.config.globalProperties.$api = 'https://my-moo.herokuapp.com/';
 
 app.use(router);
 
