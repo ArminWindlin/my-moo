0946912fc3b80200689b91ba80de0401950e3a50
ArminWindlin <armin.windlin@gmail.com>
2020-11-03 11:48:46 +0100

    Google Analytics Test

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import VueGtag from "vue-gtag-next";
 
 import './scss/reset.scss';
 import './scss/global.scss';
@@ -12,5 +13,12 @@ app.config.globalProperties.$api = 'http://localhost:3000/';
 if (process.env.NODE_ENV === 'production') app.config.globalProperties.$api = 'https://my-moo.herokuapp.com/';
 
 app.use(router);
+
+console.log('gtag setup');
+app.use(VueGtag, {
+  property: {
+    id: "G-KR0J2P5GH8"
+  }
+});
 
 app.mount('#app');
